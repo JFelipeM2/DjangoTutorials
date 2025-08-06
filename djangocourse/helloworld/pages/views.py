5fe202ef7fc5a623ff5792c13e19b772b2629a7a
JFMartine2 <juanferep59@gmail.com>
2025-08-05 21:31:56 -0500

    Add About and Contact pages with corresponding views and templates

--- djangocourse/helloworld/pages/views.py
+++ djangocourse/helloworld/pages/views.py
@@ -5,6 +5,28 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
 
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is an about page ...",
+            "author": "Developed by: Juan Felipe Martinez",
+        })
+        return context
 
-
-
+class ContactPageView(TemplateView):
+    template_name = 'pages/contact.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "Contact us - Online Store",
+            "email": "contact@onlinestore.com",
+            "address": "123 Online Store St, E-commerce City, EC 12345",
+            "phone": "+1 (555) 123-4567",
+        })
+        return context
